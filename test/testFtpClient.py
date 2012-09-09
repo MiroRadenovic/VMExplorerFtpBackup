1aba430eb27785fa620b0d3eddfbcfe70b8a9b70
myrocode <info@myrocode.com>
2012-09-09 13:49:30 +0200

    moved ftp tests into testFtp

---
+++ test/testFtpClient.py
@@ -0,0 +1,26 @@
+import unittest
+import subprocess
+import time
+import backupManager
+
+class testFtp(unittest.TestCase):
+    '''
+    ensures that all functions related to an ftp clients are correct
+    '''
+    def setUp(self):
+        try:
+            #  twistd -n ftp -p 2000 -r VMbackupFolder --password-file=/home/myo/Temp/pass.dat
+            subprocess.Popen('twistd -n ftp -p 2001 -r test/VMbackupFolder/',  shell=True)
+            # let's wait 1 secs to make sure ftp server starts
+            time.sleep(1)
+        except Exception as ex:
+            self.fail("Cannot start twistd as a ftp on port 2000. more details: " + ex.message)
+    def tearDown(self):
+        try:
+            subprocess.call('killall twistd',  shell=True)
+        except Exception as ex:
+            self.fail("Cannot kill twistd as a ftp on port 2000. more details: " + ex.message)
+
+    def testConnection(self):
+        backups = backupManager.getBackupsFromFtpServer('localhost', port=2001)
+        self.assertTrue(backups != None)
--- test/testVMExplorerFtpBackup.py
+++ test/testVMExplorerFtpBackup.py
@@ -1,9 +1,9 @@
 from datetime import datetime
-import subprocess
+
 import VMExplorerFtpBackup
 import backupManager
 import unittest
-import time
+
 from mock import patch
 
 def dateFromString(date):
@@ -210,22 +210,4 @@ class testVMExplorerFtpBackup(unittest.TestCase):
             }
             VMExplorerFtpBackup.syncBackupsToFtp('/', localBackups)
 
-class testFtp(unittest.TestCase):
-    def setUp(self):
-        try:
-            #  twistd -n ftp -p 2000 -r VMbackupFolder --password-file=/home/myo/Temp/pass.dat
-            subprocess.Popen('twistd -n ftp -p 2001 -r test/VMbackupFolder/',  shell=True)
-            # let's wait 1 secs to make sure ftp server starts
-            time.sleep(1)
-        except Exception as ex:
-            self.fail("Cannot start twistd as a ftp on port 2000. more details: " + ex.message)
-    def tearDown(self):
-        try:
-            subprocess.call('killall twistd',  shell=True)
-        except Exception as ex:
-            self.fail("Cannot kill twistd as a ftp on port 2000. more details: " + ex.message)
-
-    def testConnection(self):
-        backups = backupManager.getBackupsFromFtpServer('localhost', port=2001)
-        self.assertTrue(backups != None)
 
